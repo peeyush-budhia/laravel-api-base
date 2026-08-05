518ae6c45d0c4bf2b9b2aea562773f3b10a89a88
Peeyush Budhia <peeyush.budhia@gmail.com>
2026-08-05 15:07:34 +0000

    feat: add base application classes

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class BaseModel extends Model
+{
+    use HasUuids;
+
+    /**
+     * The primary key type.
+     */
+    protected $keyType = 'string';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     */
+    public $incrementing = false;
+}
